533e64c411b8997bfb71261cc266b8b3f39133f0
Rasmus Hald Nielsen <Rasmus.hald.nielsen@outlook.dk>
2022-03-29 10:16:25 +0200

    Creation of mongo projectSchema

--- src/app/Models/Project.js
+++ src/app/Models/Project.js
@@ -3,13 +3,86 @@ const mongo = require('mongoose');
 const Schema = mongo.Schema;
 
 projectSchema = new Schema({
-    name: String,
-    description: String
+    Id: String,
+    title: String,
+    categories: [
+        {
+            messaging: {
+                services: {
+                    discord: {
+                        state: String,
+                        token: String,
+                        serverlink: String,
+                    }
+                }
+            }
+        },
+        {
+            planning: {
+                services: {
+                    trello: {
+                        state: String,
+                        token: String,
+                        servicelink: String,
+                    }
+                }
+            }
+        },
+        {
+            development: {
+                services: {
+                    github: {
+                        state: String,
+                        token: String,
+                        servicelink: String,
+                    }
+                }
+            }
+        },
+        {
+            document: {
+                services: {
+                    overleaf: {
+                        state: String,
+                        token: String,
+                        servicelink: String,
+                    }
+                }
+            }
+        },
+        {
+            uml: {
+                services: {
+                    diagramsnet: {
+                        state: String,
+                        token: String,
+                        servicelink: String,
+                    }
+                }
+            }
+        },
+        {
+            filesharing: {
+                services: {
+                    discord: {
+                        state: String,
+                        token: String,
+                        serverlink: String,
+                    },
+                    github: {
+                        state: String,
+                        token: String,
+                        servicelink: String,
+                    }
+                }
+            }
+        },
+    ],
+    members: [
+        String
+    ],
 });
 
 const projectModel = mongo.model('projectModel', projectSchema);
 
-testproject = new projectModel({
-    name: 'Test Project',
-    description: 'This is a test project'
-});
+
