f2237c2dd48f8e27f4de99a688a8369ca97aa99f
Rasmus Hald Nielsen <Rasmus.hald.nielsen@outlook.dk>
2022-03-31 13:44:52 +0200

    newUser and getUser somewhat functions

--- src/app/Http/UserController.js
+++ src/app/Http/UserController.js
@@ -1,11 +1,42 @@
 const User = require('../Models/User');
 const bcrypt = require('bcrypt');
 
-exports.index = (req, res) => {
+function index(req, res) {
     res.render('users/index');
 }
 
-function newUser(userName, email, hashedPassword) {
-    const user = new User( { name: userName, email: email, HashedPassword: hashedPassword } );
-    user.save();
+async function newUser(userName, email, hashedPassword) {
+
+    // Check if the user already exists.
+    const userData = await getUser(userName);
+    if (userData !== null) {
+        throw new Error('User already exists.');
+    }
+
+    // Create a new user
+    const user = new User({ name: userName, email: email, hashedPassword: hashedPassword });
+    // Save the user to the database
+    await user.save();
+
 }
+
+async function getUser(userName) {
+    // Find the user in the database
+
+    const userData = await User.findOne({ name: userName }).exec();
+
+
+    return userData;
+}
+
+let userName = 'testUser';
+
+newUser(userName, 'test@test.test', 'testPassword');
+
+
+
+module.exports = {
+    index,
+    newUser,
+    getUser,
+};
