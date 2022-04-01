184c3e11ea420e312c53b0635f72dc36bd0df2c2
KB885 <coderdude92@protonmail.com>
2022-04-01 10:33:35 +0200

    ESLint - Standard

--- src/app/Models/User.js
+++ src/app/Models/User.js
@@ -1,25 +1,25 @@
-const mongo = require('mongoose');
+const mongo = require('mongoose')
 
-const Schema = mongo.Schema;
+const Schema = mongo.Schema
 
 const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    },
-});
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
 
-const User = mongo.model("User", UserSchema);
-module.exports = User;
+const User = mongo.model('User', UserSchema)
+module.exports = User
