39af5b02deed3a0f9e7e65f7d93145c274bd7b80
Rasmus Hald Nielsen <Rasmus.hald.nielsen@outlook.dk>
2022-04-01 13:39:52 +0200

    temp

--- src/app/Http/ProjectController.js
+++ src/app/Http/ProjectController.js
@@ -1,6 +1,11 @@
 const projectModel = require('../Models/Project');
 const Project = require('../Models/Project');
+const userController = require('./UserController');
 const uid = require('uid-safe');
+const User = require('../Models/User');
+
+// ! Included for debugging purposes.
+const authenticationController = require('./AuthenticationController');
 
 function project(req, res) {
     //res.render('project');
@@ -28,9 +33,18 @@ function newProject(projectName, UserID) {
     // Add initial user to the service.
     project.members.push(UserID);
 
+    // Add the project to the user's project list.
+    userController.getUser(UserID).then(user => {
+        user.projects.push(project.Id);
+        user.save();
+    });
+
+
     // Save the project to the database.
     project.save();
 }
+
+
 
 // Modules to export for testing purposes.
 module.exports = {
