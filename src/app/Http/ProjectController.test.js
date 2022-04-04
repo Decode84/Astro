8b49f0c7b505cd9cc8aa7ae8150a9c2713e0a652
soft-MRJ <mikeriberj@gmail.com>
2022-04-04 13:34:34 +0200

    Created test to create and find project by id

--- src/app/Http/ProjectController.test.js
+++ src/app/Http/ProjectController.test.js
@@ -6,29 +6,20 @@ test('recieves a 24 lenght id string', () => {
     expect(projectController.generateProjectId().length).toBe(24);
 });
 
-test('creation of a new project and finding it', async () => {
-    let userId = userController.getUser('testUser').userName;
-    let projectName = 'Test Project';
-    projectController.newProject(projectName, userId);
-    let project = await projectController.getProjectById(Project.Id);
-    project.then(project => {
-        expect(project.name).toBe(projectName);
-    });
+test('creation of a new project and finding it', () => {
+        // Create user
+        let userName = 'testUser';
+        let email = 'test@test.test';
+        let hashedPassword = 'testPassword';
+        userController.newUser(userName, email, hashedPassword);
+        let user = userController.getUser(userName);
 
+   
+        let projectName = 'Test Project';
+        projectController.newProject(projectName, user.userName);
+        let project = projectController.getProjectById(Project.Id);
+        project.then(project => {
+            expect(project.name).toBe(projectName);
+        });
+    
 });
-
-/*
-
-test('creation of a new user and finding it', () => {
-    let userName = 'testUser';
-    let email = 'test@test.test';
-    let hashedPassword = 'testPassword';
-    userController.newUser(userName, email, hashedPassword);
-    let user = userController.getUser(userName);
-    user.then(user => {
-        expect(user.userName).toBe(userName);
-        expect(user.email).toBe(email);
-        expect(user.hashedPassword).toBe(hashedPassword);
-    });
-});
-*/
