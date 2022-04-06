f97aa6f42637dcc008efcb1272e965184c8b7b23
Jakob <78407625+KB885@users.noreply.github.com>
2022-04-06 14:22:34 +0200

    Update web.js

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,6 +1,5 @@
 const router = require('express').Router()
 const auth = require('../app/Http/AuthenticationController')
-const project = require('../app/Http/ProjectController')
 const admin = require('../app/Http/Admin/AdminController')
 const middleware = require('../app/Middleware/Authorization')
 
@@ -16,9 +15,6 @@ router.get('/forgot', auth.showForgot)
 router.post('/authenticate', auth.authenticate)
 router.post('/signup', auth.signup)
 router.post('/logout', middleware.authLogin, auth.logout)
-
-// Project
-router.get('/project/overview', project.showOverview)
 
 // Admin (TODO: check for role)
 router.get('/admin/board', middleware.authLogin, admin.showBoard)
