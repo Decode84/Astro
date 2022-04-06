e349d48769d9a5946a2afb86714757070e9c8d7b
Rasmus Hald Nielsen <Rasmus.hald.nielsen@outlook.dk>
2022-04-06 14:17:32 +0200

    Removed the depricated newUser function

--- src/app/Http/UserController.js
+++ src/app/Http/UserController.js
@@ -5,22 +5,6 @@ function index(req, res) {
     res.render('users/index');
 }
 
-// ! Deprecated. The user is registered by the authentication controller.
-async function newUser(userName, email, hashedPassword) {
-
-    // Check if the user already exists.
-    const userData = await getUser(userName);
-    if (userData !== null) {
-        throw new Error('User already exists.');
-    }
-
-    // Create a new user
-    const user = new User({ name: userName, email: email, hashedPassword: hashedPassword });
-    // Save the user to the database
-    await user.save();
-
-}
-
 async function getUser(UserID) {
     // Find the user in the database
 
