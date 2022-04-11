c12a4947e1ec9f70a8492b35ced50bdd517b4d96
Rasmus Hald Nielsen <Rasmus.hald.nielsen@outlook.dk>
2022-04-11 13:46:36 +0200

    A new organization can be created using the trello api

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,7 +1,10 @@
 const router = require('express').Router()
 const auth = require('../app/Http/AuthenticationController')
 const admin = require('../app/Http/Admin/AdminController')
+const ProjectController = require('../app/Http/ProjectController');
+const TrelloAPI = require('../trello/trelloApi');
 const middleware = require('../app/Middleware/Authorization')
+
 
 /**
  * This web file is the router used to describe the correspondence
@@ -18,5 +21,11 @@ router.post('/logout', middleware.authLogin, auth.logout)
 
 // Admin (TODO: check for role)
 router.get('/admin/board', middleware.authLogin, admin.showBoard)
+
+// Project
+router.get('/project', ProjectController.project);
+
+// Trello
+router.get('/trello', TrelloAPI.trello);
 
 module.exports = router
---
+++ src/trello/trelloApi.js
@@ -0,0 +1,56 @@
+const fetch = require('node-fetch');
+const userController = require('../app/Http/UserController');
+const projectController = require('../app/Http/ProjectController');
+const Project = require('../app/Models/Project');
+const { text } = require('express');
+
+function trello(req, res) {
+
+}
+
+
+
+/*
+function authUser() {
+    fetch('https://trello.com/1/authorize?expiration=never&name=Project_Hub&scope=read&response_type=token&key=e5b8e9efa5bf84e76b15d443eb9b5afc')
+}
+*/
+/**
+ * 
+ * @param {String} name The name of the organization to be created.
+ * @param {String} userId The id of the user who is creating the organization.
+ * @param {String} projectId The id of the project that the organization is being created for.
+ */
+async function newOrganization(name, userId, projectId) {
+    let organizationId;
+    const response = await fetch('https://api.trello.com/1/organizations?displayName=' + name + '&key=' + process.env.TRELLO_KEY + '&token=' + process.env.TRELLO_TOKEN, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+        }
+    })
+
+    const text = await response.text();
+
+    const json = JSON.parse(text);
+
+    organizationId = json.id;
+
+
+    let project = await projectController.getProjectById(projectId);
+
+    let service = {...project.categories.planning.services};
+    service.trello.organizationId = organizationId;
+    service.update = !service.update;
+
+    project.categories.planning.services = service;
+
+    console.log(project.categories.planning.services);
+
+    await project.save();
+}
+
+module.exports = {
+    trello,
+    newOrganization
+}
