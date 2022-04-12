fc2d69a2177ebdb8b2998d175f6b02a2fc8b2274
KB885 <coderdude92@protonmail.com>
2022-04-12 10:42:11 +0200

    Security related

--- src/routes/web.js
+++ src/routes/web.js
@@ -3,6 +3,7 @@ const auth = require('../app/Http/AuthenticationController')
 const admin = require('../app/Http/Admin/AdminController')
 const middleware = require('../app/Middleware/Authorization')
 const dash = require('../app/Http/ProjectController')
+const { createAccountLimit, loginLimit } = require('../app/Middleware/Rate')
 const { authenticateValidation, registerValidation } = require('../app/Validation/AuthValidation')
 
 /**
@@ -14,8 +15,8 @@ const { authenticateValidation, registerValidation } = require('../app/Validatio
 router.get('/login', auth.showLogin)
 router.get('/register', auth.showRegister)
 router.get('/forgot', auth.showForgot)
-router.post('/authenticate', authenticateValidation, auth.authenticate)
-router.post('/signup', registerValidation, auth.signup)
+router.post('/authenticate', loginLimit, authenticateValidation, auth.authenticate)
+router.post('/signup', createAccountLimit, registerValidation, auth.signup)
 router.post('/logout', middleware.authLogin, auth.logout)
 
 // Project
