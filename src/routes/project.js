014e1205f1c8489bacc8b10e5551556dc881c57d
williamwoldum <wilwoldum@gmail.com>
2022-04-19 09:31:38 +0200

    changed web.js to routes.js + deleted old relics + small html adjustments

--- src/routes/project.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const ProjectController = require('../app/Http/ProjectController')
-
-router.get('/', ProjectController.project)
-
-module.exports = router
