17436fb229fcee9d4f5e2ed53da8453c4b4c5ed6
KB885 <coderdude92@protonmail.com>
2022-04-27 13:27:17 +0200

    hotfix

--- src/app/Http/GithubController.js
+++ src/app/Http/GithubController.js
@@ -1,8 +1,8 @@
-const { getPackageJSON, getIssues } = require('./github/githubApi')
+const { getPackageJSON, getIssues } = require('../../github/githubApi')
 
 class GithubController {
     async hook (req) {
-        if (req.body != null) {
+        if (req.body !== null) {
             const installationId = req.body.installation.id
             const log = getPackageJSON(req.body.repository.full_name, installationId)
             // getIssues(req.body.repository.issues, installationId)
