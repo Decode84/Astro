53a784fcfb2663cb01ec6252b8bbcd9bb3514718
Aavild <46568674+Aavild@users.noreply.github.com>
2022-04-28 13:28:00 +0200

    Temporarily disabled on invite to guild. /link command still works

--- src/discord/GuildCreateHandler.js
+++ src/discord/GuildCreateHandler.js
@@ -1,11 +1,11 @@
 const Linker = require('./DiscordLinker')
 // https://discord.js.org/#/docs/main/stable/class/ClientUser?scrollTo=send
-async function OnGuildCreate(guild) {
+async function OnGuildCreate (guild) {
     // TODO: make sure that the bot has all the needed perms
-    const auditLogs = await guild.fetchAuditLogs() // auditLogs is the discord servers log for everything the admins do
+    /* const auditLogs = await guild.fetchAuditLogs() // auditLogs is the discord servers log for everything the admins do
     const user = await auditLogs.entries.first().executor // We know this is the admin who added the bot
     await user
-    await Linker.Link(guild, user)
+    await Linker.Link(guild, user) */ // TODO: fix so that it sends ephemeral message to user that first prompts channel and then project to link to
 }
 
 module.exports = { OnGuildCreate }
