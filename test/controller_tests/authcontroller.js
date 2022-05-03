30a5d72a061f4a073f133a04a8a9b2b90bc95fdd
Rasmus Hald Nielsen <Rasmus.hald.nielsen@outlook.dk>
2022-05-03 08:40:45 +0200

    Start of authcontroller

---
+++ test/controller_tests/authcontroller.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const request = require('supertest')
+const assert = require('assert')
+const expressApp = require('../../src/server.js')
+const authController = require('../../src/app/Http/AuthenticationController.js')
+
+
+beforeEach((done) => {
+    
+})
