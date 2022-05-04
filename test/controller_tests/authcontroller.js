ff1738fa0ed33158fe0f50c49db658705e30c6d1
Rasmus Hald Nielsen <Rasmus.hald.nielsen@outlook.dk>
2022-05-04 11:50:57 +0200

    stash

--- test/controller_tests/authcontroller.js
+++ test/controller_tests/authcontroller.js
@@ -1,10 +1,93 @@
+/* eslint-disable no-undef */
 const mongoose = require('mongoose')
+const mongoStore = require('connect-mongo')
 const request = require('supertest')
 const assert = require('assert')
-const expressApp = require('../../src/server.js')
+const express = require('express')
+const expressEjsLayout = require('express-ejs-layouts')
+const session = require('express-session')
 const authController = require('../../src/app/Http/AuthenticationController.js')
+const User = require('../../src/app/Models/User')
+const flash = require('express-flash')
+const cors = require('cors')
+
+const app = express()
+
+const db = mongoose.connection
+
+const sess = {
+    secret: process.env.SECRET_KEY,
+    saveUninitialized: false, // don't create session until something stored
+    resave: false, // don't save session if unmodified
+    // rolling: true, //Reset the cookie Max-Age on every request
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000, // 24 hours
+        secure: false // true for https
+    },
+    store: mongoStore.create({
+        mongoUrl: db._connectionString,
+        ttl: 14 * 24 * 60 * 60, // = 14 days. Default
+        autoRemove: 'native' // Default
+        // crypto: {
+        //    secret: 'process.ENV.SECRET_KEY',
+        //    hashing: 'sha256'
+        // }
+    })
+}
+
+before((done) => {
+    app.set('views', path.join(__dirname, '../../src/resources/views'))
+    app.set('view engine', 'ejs')
+    app.use(expressEjsLayout)
+
+    app.use(session(sess))
+    app.use(flash())
+    app.use(cors())
+
+    app.use(express.json())
+    app.use(express.urlencoded({
+        extended: true
+    }))
+
+    app.use('/', express.static('public'), require('../../src/routes'))
+
+    const PORT = process.env.PRI_SERVER_PORT || process.env.SEC_SERVER_PORT
+    app.listen(PORT, (err) => {
+        if (err) console.log(err)
+    })
+    done()
+})
+
+let newUser
 
 
 beforeEach((done) => {
+    newUser = new User({
+        name: 'Test User',
+        username: 'testuser',
+        email: 'test@test.test',
+        password: '$2b$10$R.i4.m0ApkV3Em7mzpgNC.w9W9dAoJlvLJa5YjREL108m5XLt2pIO',
+        date: Date.now(),
+        services: {},
+        projectIDs: [],
+        authentications: {}
+    })
+    let save_state = newUser.save()
     
 })
+/*
+describe('Testing Authentication', () => {
+    it('User verification', (done) => {
+        request(app)
+            .get('/authenticate')
+            .send({
+                username: 'testuser',
+                password: 'testPassword'
+            })
+            .set('Accept', 'text/html')
+            .then(response => {
+                console.log(response)
+            })
+    })
+})
+*/
