a77193cc6c9495e0b96b54ec36388bc737ff2319
William Woldum <41169797+williamwoldum@users.noreply.github.com>
2022-05-11 14:26:46 +0200

    Fix caching plus some form touch up (#151)
    * fixed cashing permissions and removed deprecated role functionality
    
    * forms touch up

--- src/app/Middleware/Authorization.js
+++ src/app/Middleware/Authorization.js
@@ -8,16 +8,6 @@ class Authorization {
         }
         await next()
     }
-
-    /**
-    * Role required middleware
-    */
-    async authRole (req, res, next) {
-        if (req.user.role !== role) {
-            return res.send('Not allowed')
-        }
-        await next()
-    }
 }
 
 module.exports = new Authorization()
