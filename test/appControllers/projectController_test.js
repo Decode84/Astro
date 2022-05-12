11635d297386ab7df1e2111ecdd176e6fdbb9747
Rasmus Hald Nielsen <Rasmus.hald.nielsen@outlook.dk>
2022-05-12 10:06:19 +0200

    Test of projectController

--- test/appControllers/projectController_test.js
+++ test/appControllers/projectController_test.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-undef */
 const chai = require('chai')
 const User = require('../../src/app/Models/User')
-const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
 const Project = require('../../src/app/Models/Project')
 
 const ProjectController = require('../../src/app/Http/ProjectController')
+const { addProjectToUser } = require('../../src/app/Http/ProjectController')
 
 const assert = chai.assert
 chai.use(sinonChai)
@@ -14,15 +14,23 @@ let user
 let project
 
 describe('ProjectController', () => {
-    beforeEach((done) => {
+    beforeEach(async () => {
         user = new User({
             name: 'testProjUser',
             username: 'testProjuser',
-            email: 'test@test.test',
+            email: 'projectCon@test.test',
             password: '$2b$10$tNYovXfIfiqlbaxWUnFaAeWSE1/gsQIgW3NSNZbVEKEDYn7iF/oe2'
         })
-        user.save()
-            .then(() => done())
+        await user.save()
+        project = new Project({
+            name: 'testProj',
+            duration: {
+                startTime: new Date(2022, 1, 1),
+                endTime: new Date(2022, 1, 2)
+            },
+            members: []
+        })
+        await project.save()
     })
     describe('newProject function', () => {
         let req, res
@@ -59,6 +67,96 @@ describe('ProjectController', () => {
             res.aurl = '/project/'
 
             ProjectController.newProject(req, res)
+        })
+    })
+    describe('addProjectToUser function', () => {
+        it('should add a project to the user', async function () {
+            // Arrange
+            const project = await Project.findOne({ name: 'testProj' })
+            const user = await User.findOne({ name: 'testProjUser' })
+
+            // Act
+            await ProjectController.addProjectToUser(project._id.toString(), user._id.toString())
+
+            // Assert
+            const updatedUser = await User.findOne({ name: 'testProjUser' })
+            assert.equal(updatedUser.projectIDs.length, 1)
+            assert.equal(updatedUser.projectIDs[0], project._id.toString())
+        })
+    })
+    describe('removeProjectFromUser function', () => {
+        it('should remove a project from the user', async function () {
+            // Arrange
+            const project = await Project.findOne({ name: 'testProj' })
+            const user = await User.findOne({ name: 'testProjUser' })
+            await ProjectController.addProjectToUser(project._id.toString(), user._id.toString())
+
+            // Act
+            await ProjectController.removeProjectFromUser(project._id.toString(), user._id.toString())
+
+            // Assert
+            const updatedUser = await User.findOne({ name: 'testProjUser' })
+            assert.equal(updatedUser.projectIDs.length, 0)
+        })
+    })
+    describe('isUserInProject', () => {
+        it('should return true if the user is in the project', async function () {
+            // Arrange
+            const project = await Project.findOne({ name: 'testProj' })
+            const user = await User.findOne({ name: 'testProjUser' })
+
+            await ProjectController.addProjectToUser(project._id.toString(), user._id.toString())
+            project.members.push(user._id.toString())
+            await project.save()
+
+            const updatedProject = await Project.findById(project._id.toString())
+            // Act
+            const result = await ProjectController.isUserInProject(updatedProject, user._id.toString())
+
+            // Assert
+            assert.equal(result, true)
+        })
+
+        it('should return false if the user is not in the project', async function () {
+            // Arrange
+            const project = await Project.findOne({ name: 'testProj' })
+            const user = await User.findOne({ name: 'testProjUser' })
+
+            // Act
+            const result = await ProjectController.isUserInProject(project, user._id.toString())
+
+            // Assert
+            assert.equal(result, false)
+        })
+    })
+    describe('delProject function', () => {
+        let req, res
+        beforeEach(async () => {
+            req = {
+                body: {
+                    projectID: ''
+                }
+            }
+            res = {
+                redirect: function (url) {
+                }
+            }
+        })
+        it('should delete the project', async function () {
+            // Arrange
+            const project = await Project.findOne({ name: 'testProj' })
+            const user = await User.findOne({ name: 'testProjUser' })
+            req.body.projectId = project._id.toString()
+            await addProjectToUser(project._id.toString(), user._id.toString())
+            project.members.push(user._id.toString())
+            await project.save()
+
+            // Act
+            await ProjectController.delProject(req, res)
+
+            // Assert
+            const deletedProject = await Project.findById(project._id.toString())
+            assert.equal(deletedProject, null)
         })
     })
 })
