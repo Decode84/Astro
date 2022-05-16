9e3728d9a1d219a3360abb4d4c2ff863d7744215
William Woldum <41169797+williamwoldum@users.noreply.github.com>
2022-05-16 12:57:42 +0200

    fixed email redirect (#173)

--- src/app/Http/MailController.js
+++ src/app/Http/MailController.js
@@ -4,7 +4,7 @@ const path = require('path')
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 
 class MailController {
-    sendEmail(email, token) {
+    sendResetEmail(email, token, path) {
         const mail = nodemailer.createTransport({
             service: 'gmail',
             auth: {
@@ -12,12 +12,12 @@ class MailController {
                 pass: process.env.MAIL_KEY
             }
         })
-        console.log(mail)
+
         const mailOptions = {
             from: 'ProjectHub.AAU@gmail.com',
             to: email,
             subject: 'Reset Password Link - ProjectHub.com',
-            html: '<p>You requested for reset password, kindly use this <a href="http://localhost:4000/reset?token=' + token + '">link</a> to reset your password</p>'
+            html: '<p>You requested for reset password, kindly use this <a href="http://' + path + '/reset?token=' + token + '">link</a> to reset your password</p>'
 
         }
 
