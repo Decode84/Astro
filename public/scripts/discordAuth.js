dd544664968bebe348a3d448beebf2fcfb2fc60d
Aavild <46568674+Aavild@users.noreply.github.com>
2022-05-17 12:18:32 +0200

    Unused localhost replace (#192)
    * Unused localhost replace
    
    * More code that doesn't do anything
    
    * More code that doesn't do anything
    
    * why dis back
    
    * no cookie either
    
    * retry on cleanup
    
    * Bad friends with rebase...

--- public/scripts/discordAuth.js
+++ public/scripts/discordAuth.js
@@ -23,10 +23,10 @@ if (!discordCode) {
 function generateRandomString () {
     let randomString = ''
     const randomNumber = Math.floor(Math.random() * 10)
-
+    
     for (let i = 0; i < 20 + randomNumber; i++) {
         randomString += String.fromCharCode(33 + Math.floor(Math.random() * 94))
     }
-
+    
     return randomString
 }
--- public/scripts/githubAuth.js
+++ public/scripts/githubAuth.js
@@ -1,28 +1,12 @@
-// console.log(window.location) //http://localhost:4000/discord?code=SUysVwZaIztTpdkvVzNe6RCZn3fRqC
-
 const githubUrl = new URL(window.location)
-const githubCode = githubUrl.searchParams.get('code') // SUysVwZaIztTpdkvVzNe6RCZn3fRqC
-// before auth
-if (!githubCode) {
-    const randomString = generateRandomString()
-    localStorage.setItem('oauth-state', randomString)
-    let githref = document.getElementById('githubLogin').href
-    githref = githref.replace('localhost%3A4000', encodeURIComponent(githubUrl.host))
-    githref += `&state=${btoa(randomString)}::` + githubUrl.pathname.substring(githubUrl.pathname.lastIndexOf('/') + 1)
-    document.getElementById('githubLogin').href = githref
-    
-    document.getElementById('githubLogin').style.display = 'block'
-}
-else {
-    // after auth
-    const state = githubUrl.searchParams.get('state')
-    if (localStorage.getItem('oauth-state') !== atob(decodeURIComponent(state))) {
-        // May have been clickjacked
-        document.getElementById('githubInfo').innerText = 'Failed to authenticate. Try again'
-    }
-    else
-        document.getElementById('githubInfo').innerText = 'Github account linked'
-}
+const randomString = generateRandomString()
+
+let githref = document.getElementById('githubLogin').href
+githref += `&state=${btoa(randomString)}::` + githubUrl.pathname.substring(githubUrl.pathname.lastIndexOf('/') + 1)
+document.getElementById('githubLogin').href = githref
+
+document.getElementById('githubLogin').style.display = 'block'
+
 function generateRandomString () {
     let randomString = ''
     const randomNumber = Math.floor(Math.random() * 10)
