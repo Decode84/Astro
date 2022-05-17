e9343e846e95966bd43df3625e4266c7cd115339
Aavild <anders@aavild.dk>
2022-05-17 11:47:54 +0200

    More code that doesn't do anything

--- public/scripts/discordAuth.js
+++ public/scripts/discordAuth.js
@@ -1,25 +1,13 @@
-// console.log(window.location) //http://localhost:4000/discord?code=SUysVwZaIztTpdkvVzNe6RCZn3fRqC
 const discordUrl = new URL(window.location)
-const discordCode = discordUrl.searchParams.get('code') // SUysVwZaIztTpdkvVzNe6RCZn3fRqC
-// before auth
-if (!discordCode) {
-    const randomString = generateRandomString()
-    localStorage.setItem('oauth-state', randomString)
-    let href = document.getElementById('login').href
-    href = href.replace('localhost%3A4000', encodeURIComponent(discordUrl.host))
-    href += `&state=${btoa(randomString)}::` + discordUrl.pathname.substring(discordUrl.pathname.lastIndexOf('/') + 1)
-    document.getElementById('login').href = href
-    document.getElementById('login-container').classList.remove('hidden')
-} else {
-    // after auth
-    const state = discordUrl.searchParams.get('state')
-    if (localStorage.getItem('oauth-state') !== atob(decodeURIComponent(state))) {
-        // May have been clickjacked
-        document.getElementById('info').innerText = 'Failed to authenticate. Try again'
-    } else {
-        document.getElementById('info').innerText = 'Discord account linked'
-    }
-}
+const randomString = generateRandomString()
+
+localStorage.setItem('oauth-state', randomString)
+let href = document.getElementById('login').href
+href = href.replace('localhost%3A4000', encodeURIComponent(discordUrl.host))
+href += `&state=${btoa(randomString)}::` + discordUrl.pathname.substring(discordUrl.pathname.lastIndexOf('/') + 1)
+document.getElementById('login').href = href
+document.getElementById('login-container').classList.remove('hidden')
+
 function generateRandomString () {
     let randomString = ''
     const randomNumber = Math.floor(Math.random() * 10)
