3726a4bb3f3f0b0cb46b8b8ee47580c50ae20015
Aavild <anders@aavild.dk>
2022-05-17 11:56:23 +0200

    retry on cleanup

--- public/scripts/githubAuth.js
+++ public/scripts/githubAuth.js
@@ -1,17 +1,12 @@
-// console.log(window.location) //http://localhost:4000/discord?code=SUysVwZaIztTpdkvVzNe6RCZn3fRqC
-
 const githubUrl = new URL(window.location)
-const githubCode = githubUrl.searchParams.get('code') // SUysVwZaIztTpdkvVzNe6RCZn3fRqC
-// before auth
-if (!githubCode) {
-    const randomString = generateRandomString()
-    localStorage.setItem('oauth-state', randomString)
-    let githref = document.getElementById('githubLogin').href
-    githref += `&state=${btoa(randomString)}::` + githubUrl.pathname.substring(githubUrl.pathname.lastIndexOf('/') + 1)
-    document.getElementById('githubLogin').href = githref
-    
-    document.getElementById('githubLogin').style.display = 'block'
-}
+const randomString = generateRandomString()
+
+let githref = document.getElementById('githubLogin').href
+githref += `&state=${btoa(randomString)}::` + githubUrl.pathname.substring(githubUrl.pathname.lastIndexOf('/') + 1)
+document.getElementById('githubLogin').href = githref
+
+document.getElementById('githubLogin').style.display = 'block'
+
 function generateRandomString () {
     let randomString = ''
     const randomNumber = Math.floor(Math.random() * 10)
