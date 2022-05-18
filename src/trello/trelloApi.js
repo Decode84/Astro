41ad398c136eabb4e102e76c78d8a4ac18fb9b4d
Rasmus Hald Nielsen <Rasmus.hald.nielsen@outlook.dk>
2022-05-18 09:37:27 +0200

    test

--- src/trello/trelloApi.js
+++ src/trello/trelloApi.js
@@ -149,7 +149,7 @@ const TrelloApi = {
                 Accept: 'application/json'
             }
         })
-        res.send(response.text())
+        res.send(organizationOwner.text())
         //res.redirect('/project/' + projectId)
     },
 
